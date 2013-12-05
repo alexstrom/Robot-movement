b2e02d0d62d2eb6ba95266c9fcad3631b9bf3d8b
alexstrom <c15alex.strom@usafa.edu>
2013-12-05 11:23:07 -0700

    the header file

---
+++ robot.h
@@ -0,0 +1,27 @@
+/*
+ * robot.h
+ *
+ *  Created on: Nov 19, 2013
+ *      Author: C15Alex.Strom
+ */
+
+#ifndef ROBOT_H_
+#define ROBOT_H_
+
+void moveRightMotorF();
+void moveLeftMotorF();
+void moveRightMotorB();
+void moveLeftMotorB();
+
+void moveForward();
+void moveBack();
+
+void moveRight();
+void moveLeft();
+
+void initRobot();
+void initTimer();
+
+
+
+#endif /* ROBOT_H_ */
